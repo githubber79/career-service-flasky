b46042d9098cb43d986bc21b0f26ce9509c2831c
jagadish pamidimalla <31544579+githubber79@users.noreply.github.com>
2022-09-11 07:42:30 +0000

    added a new route

--- src/app.py
+++ src/app.py
@@ -4,10 +4,15 @@ from flask import jsonify
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return "<h1> Hello world in Flask </h1>"
+@app.route("/", methods=['GET', 'POST'])
+def home(name="Guest User"):
+    return f"<h1> Hello {name} in Flask </h1>"
 
 
-if "__name__" == "__main__":
-    app.run()
+@app.route("/json")
+def json():
+    return jsonify({"key1": "value1", "key2": [1, 2, 3, 4, 5]})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
